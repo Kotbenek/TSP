b1b236873029e29252a1f5f877e687aec68f9247
Kotbenek <dominik.piatkowski@protonmail.com>
2021-02-17 19:23:48 +0100

    Implemented loading EUC_3D-type instances

--- TSP/Instance.cpp
+++ TSP/Instance.cpp
@@ -179,7 +179,24 @@ Instance::Instance(string filename)
 		}
 		else if (edge_weight_type == "EUC_3D")
 		{
-			throw new exception("not implemented");
+			double** coords = parse_coords(file, i, 3);
+
+			for (int j = 0; j < size; j++)
+			{
+				for (int k = 0; k < size; k++)
+				{
+					if (j == k) { matrix[j][k] = -1; continue; }
+
+					double xd = coords[j][0] - coords[k][0];
+					double yd = coords[j][1] - coords[k][1];
+					double zd = coords[j][2] - coords[k][2];
+
+					matrix[j][k] = nint(sqrt(xd * xd + yd * yd + zd * zd));
+				}
+			}
+
+			for (int i = 0; i < size; i++) delete[] coords[i];
+			delete[] coords;
 		}
 		else if (edge_weight_type == "MAX_2D")
 		{
