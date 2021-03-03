3dade6a8f755b96c371ddbe68fc4e436bf27268f
Kotbenek <dominik.piatkowski@protonmail.com>
2021-03-03 08:23:02 +0100

    Implemented genetic algorithm, argument parsing improvements

--- TSP/TSP.cpp
+++ TSP/TSP.cpp
@@ -9,7 +9,7 @@ int main(int argc, char** argv)
     if (argc < 3)
     {
         cout << "\r\n";
-        cout << "Usage: TSP.exe [-d] [-gr] [-gr2] [-bf] [-sa T1 T2 el cf nt] filename" << "\r\n";
+        cout << "Usage: TSP.exe [-d] [-gr] [-gr2] [-bf] [-sa T1 T2 el cf nt] [-ga ps mi mf cf pc] filename" << "\r\n";
 
         cout << "\r\n";
 
@@ -24,6 +24,12 @@ int main(int argc, char** argv)
         cout << "       el" << "         " << "Epoch length" << "\r\n";
         cout << "       cf" << "         " << "Cooling factor (geometric cooling scheme)" << "\r\n";
         cout << "       nt" << "         " << "Neighbourhood type" << "\r\n";
+        cout << "    -ga" << "        " << "Solve using genetic algorithm" << "\r\n";
+        cout << "       ps" << "         " << "Population size" << "\r\n";
+        cout << "       mi" << "         " << "Max iterations with no improvement" << "\r\n";
+        cout << "       mf" << "         " << "Mutation factor" << "\r\n";
+        cout << "       cf" << "         " << "Crossover factor" << "\r\n";
+        cout << "       pc" << "         " << "Percentage of population to crossover" << "\r\n";
         cout << "\r\n";
         cout << "Neighbourhood types:" << "\r\n";
         cout << "    swp" << "        " << "Swap" << "\r\n";
@@ -84,25 +90,25 @@ int main(int argc, char** argv)
                 double cooling_factor;
                 void (*neighbourhood_type)(int* tour, int i1, int i2);
 
-                if (!StringFunctions::to_double(t1, &T_start))
+                if (!StringFunctions::to_double(t1, &T_start) || T_start <= 0.0)
                 {
                     cout << "Bad argument t1: \"" << t1 << "\"." << "\r\n";
                     continue;
                 }
 
-                if (!StringFunctions::to_double(t2, &T_end))
+                if (!StringFunctions::to_double(t2, &T_end) || T_end <= 0.0)
                 {
                     cout << "Bad argument t2: \"" << t2 << "\"." << "\r\n";
                     continue;
                 }
                 
-                if (!StringFunctions::to_int(el, &epoch_length))
+                if (!StringFunctions::to_int(el, &epoch_length) || epoch_length <= 0)
                 {
                     cout << "Bad argument el: \"" << el << "\"." << "\r\n";
                     continue;
                 }
 
-                if (!StringFunctions::to_double(cf, &cooling_factor))
+                if (!StringFunctions::to_double(cf, &cooling_factor) || cooling_factor <= 0.0 || cooling_factor >= 1.0)
                 {
                     cout << "Bad argument cf: \"" << cf << "\"." << "\r\n";
                     continue;
@@ -125,6 +131,68 @@ int main(int argc, char** argv)
                 cout << "\r\n";
 
                 Algorithms::simulated_annealing(instance, T_start, T_end, epoch_length, cooling_factor, neighbourhood_type);                
+            }
+            else if (arg == "-ga")
+            {
+                cout << "Genetic algorithm" << "\r\n";
+
+                if (argc - 2 - i < 5)
+                {
+                    cout << "Not enough arguments for \"-ga\"." << "\r\n";
+                    break;
+                }
+
+                string ps = argv[i + 1];
+                string mi = argv[i + 2];
+                string mf = argv[i + 3];
+                string cf = argv[i + 4];
+                string pc = argv[i + 5];
+                i += 5;
+
+                int population_size;
+                int max_iterations_with_no_improvement;
+                double mutation_factor;
+                double crossover_factor;
+                int percentage_of_population_to_crossover;
+
+                if (!StringFunctions::to_int(ps, &population_size) || population_size <= 0)
+                {
+                    cout << "Bad argument ps: \"" << ps << "\"." << "\r\n";
+                    continue;
+                }
+
+                if (!StringFunctions::to_int(mi, &max_iterations_with_no_improvement) || max_iterations_with_no_improvement <= 0)
+                {
+                    cout << "Bad argument mi: \"" << mi << "\"." << "\r\n";
+                    continue;
+                }
+
+                if (!StringFunctions::to_double(mf, &mutation_factor) || mutation_factor <= 0 || mutation_factor >= 1)
+                {
+                    cout << "Bad argument mf: \"" << mf << "\"." << "\r\n";
+                    continue;
+                }
+
+                if (!StringFunctions::to_double(cf, &crossover_factor) || crossover_factor <= 0 || crossover_factor >= 1)
+                {
+                    cout << "Bad argument cf: \"" << cf << "\"." << "\r\n";
+                    continue;
+                }
+
+                if (!StringFunctions::to_int(pc, &percentage_of_population_to_crossover) || percentage_of_population_to_crossover < 1 || percentage_of_population_to_crossover > 100)
+                {
+                    cout << "Bad argument pc: \"" << pc << "\"." << "\r\n";
+                    continue;
+                }
+
+                cout << "   ps: " << population_size << "\r\n";
+                cout << "   mi: " << max_iterations_with_no_improvement << "\r\n";
+                cout << "   mf: " << mutation_factor << "\r\n";
+                cout << "   cf: " << crossover_factor << "\r\n";
+                cout << "   pc: " << percentage_of_population_to_crossover << "\r\n";
+                cout << "\r\n";
+
+                Algorithms::genetic(instance, population_size, max_iterations_with_no_improvement, mutation_factor, crossover_factor, percentage_of_population_to_crossover);
             }
             else
             {
